b3ccb3ecd8a8ff3bee7f54c0d30e1101f7040fea
happygamja <silvertrhead326@gmail.com>
2024-11-08 22:48:46 +0900

    1. 목록화면을 메인화면 페이지로 변경 2. BottomNavigationView 구현을 위해 fragment로 변경 3. navigation 클릭시 클릭 이벤트 추가

--- app/src/main/java/com/example/soltraveldaiary/MainActivity.kt
+++ app/src/main/java/com/example/soltraveldaiary/MainActivity.kt
@@ -3,18 +3,40 @@ package com.example.soltraveldaiary
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.soltraveldaiary.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        initNavigationBar()
+    }
+
+    fun initNavigationBar() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_content) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController = findNavController(R.id.main_content)
+
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
